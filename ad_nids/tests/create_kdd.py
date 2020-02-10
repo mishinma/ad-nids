e6a328ebdc970383b74c131e72e91d6473fa54d9
Mikhail Mishin <mishinma1805@gmail.com>
2020-02-10 12:27:18 +0200

    kdd name

--- ad_nids/tests/create_kdd.py
+++ ad_nids/tests/create_kdd.py
@@ -34,7 +34,6 @@ test_df = pd.DataFrame(X_test)
 test_df['lbl'] = y_test
 
 meta = dict(
-    name='KDDCUP99_{}'.format(rs),
     test_size=0.33,
     random_state=rs,
     percent10=args.percent10
@@ -44,6 +43,7 @@ name = 'KDDCUP99'
 if args.percent10:
     name +='_0p10'
 name += '_{}'.format(rs)
+meta[name] = name
 
 dataset = Dataset(train_df, test_df, meta=meta)
 dataset.write_to(dataset_path, overwrite=True, visualize=True)
