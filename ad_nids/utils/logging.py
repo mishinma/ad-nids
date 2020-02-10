edc5c05563a677cab26d2641c60c2101cafdba10
Mikhail Mishin <mishinma1805@gmail.com>
2020-02-10 12:10:09 +0200

    get log dir

--- ad_nids/utils/logging.py
+++ ad_nids/utils/logging.py
@@ -12,9 +12,10 @@ from ad_nids.utils.plot import plot_precision_recall, \
     plot_f1score, plot_data_2d, plot_frontier
 
 
-def get_log_dir(log_root_dir, config_name):
-    uniq_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f') + '_' + config_name
-    log_dir = Path(log_root_dir) / uniq_name
+def get_log_dir(log_root_dir, config):
+    tstmp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
+    log_name = f'{tstmp}_{config["experiment_name"]}_{config["dataset_name"]}_{config["config_name"]}'
+    log_dir = Path(log_root_dir) / log_name
     log_dir = log_dir.resolve()
     return log_dir
 
